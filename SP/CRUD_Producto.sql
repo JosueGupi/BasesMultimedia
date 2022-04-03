661a988b89732072db1b3228168cd1c5c3a48a84
Geisel007 <montoyageisel@estudiantec.cr>
2022-04-03 17:08:02 -0600

    manejo de errores procedimientos

--- SP/CRUD_Producto.sql
+++ SP/CRUD_Producto.sql
@@ -1,10 +1,12 @@
 -- Agregar Funcion o procedimientoLos productos se deben de poder agregar al inventario (CRUD)
 -- Flag: 1. CREATE 2. READ 3. UPDATE 4. DELETE 
 
-GO -- DROP PROCEDURE spProducto
-ALTER PROCEDURE spProducto (@flag int, @idProducto int, @idTipoMaterial int, @idTipoProducto int,
+SELECT * FROM Producto
+
+GO --DROP PROCEDURE spProducto
+ALTER PROCEDURE spProducto2 @flag int, @idProducto int, @idTipoMaterial int, @idTipoProducto int,
 							 @idDimensiones int, @precio money, @nombre varchar(16), @imagen varbinary(MAX),
-							 @color varchar(16), @cuidados varchar(256))
+							 @color varchar(16), @cuidados varchar(256)
 WITH ENCRYPTION AS
 DECLARE @error varchar(2222)
 DECLARE @aux int 
@@ -16,73 +18,87 @@ IF(SELECT COUNT(idProducto) FROM Producto WHERE idProducto = @idProducto) = 0  -
 		SELECT @error
 		RETURN 1
 	END
-IF @flag = 1 -- CREATE
-BEGIN
-	BEGIN TRY
-		INSERT INTO Producto (idTipoMaterial,idTipoProducto,idDimensiones,precio,nombre,imagen,cuidados)
-					VALUES (@idTipoMaterial,@idTipoProducto,@idDimensiones,@precio,@nombre,@imagen,@cuidados)
-		SELECT @aux = SCOPE_IDENTITY() -- obtener id del elemento recién ingresado
-		INSERT INTO Caracteristicas (idProducto,color)
-					VALUES (@aux,@color)
-		SET @error = 'Producto ingresado correctamente.'
+--BEGIN TRY 
+	IF @flag = 1 -- CREATE
+	BEGIN
+		BEGIN TRY
+			INSERT INTO Producto (idTipoMaterial,idTipoProducto,idDimensiones,precio,nombre,imagen,cuidados)
+						VALUES (@idTipoMaterial,@idTipoProducto,@idDimensiones,@precio,@nombre,@imagen,@cuidados)
+			SELECT @aux = SCOPE_IDENTITY() -- obtener id del elemento recién ingresado
+			INSERT INTO Caracteristicas (idProducto,color)
+						VALUES (@aux,@color)
+			SET @error = 'Producto ingresado correctamente.'
+			SELECT @error
+		END TRY
+		BEGIN CATCH
+			SET @error = 'Error al ingresar el Producto.'
+			SELECT @error
+			RETURN 1
+		END CATCH
+	END
+	IF @flag = 2 -- READ
+	BEGIN
+		SELECT P.idProducto, nombre, precio, imagen, cuidados, color, tipoMaterial, altura, lomgitud, profundidad, tipoProducto
+		FROM Producto AS P
+		INNER JOIN Caracteristicas
+			ON P.idProducto = Caracteristicas.idProducto
+		INNER JOIN TipoProducto
+			ON P.idTipoProducto = TipoProducto.idTipoProducto
+		INNER JOIN TipoMaterial
+			ON P.idTipoMaterial = TipoMaterial.idTipoMaterial
+		INNER JOIN Dimensiones
+			ON P.idDimensiones = Dimensiones.idDimensiones
+		WHERE P.idProducto = ISNULL(@idProducto,P.idProducto) 
+		AND   P.idTipoMaterial = ISNULL(@idTipoMaterial,P.idTipoMaterial)
+		AND   P.idTipoProducto = ISNULL(@idTipoProducto,P.idTipoProducto)
+		AND   P.idDimensiones = ISNULL(@idDimensiones,P.idDimensiones)
+		AND   precio = ISNULL(@precio,precio)
+		AND   nombre = ISNULL(@nombre,nombre)
+		AND   color = ISNULL(@color,color)
+		AND   cuidados = ISNULL(@cuidados,cuidados)
+		SET @error = 'Consulta finalizada.'
 		SELECT @error
-	END TRY
-	BEGIN CATCH
-		SET @error = 'Error al ingresar el Producto.'
+	END
+	IF @flag = 3 -- UPDATE
+	BEGIN
+		UPDATE Producto 
+		SET   idTipoMaterial = ISNULL(@idTipoMaterial,idTipoMaterial),
+			  idTipoProducto = ISNULL(@idTipoProducto,idTipoProducto),
+			  idDimensiones = ISNULL(@idDimensiones,idDimensiones),
+			  precio = ISNULL(@precio,precio),
+			  nombre = ISNULL(@nombre,nombre),
+			  cuidados = ISNULL(@cuidados,cuidados),
+			  imagen = @imagen
+		WHERE idProducto = @idProducto
+		UPDATE Caracteristicas 
+		SET   color = ISNULL(@color,color)
+		WHERE idProducto = @idProducto
+		SET @error = 'El producto se ha actualizado correctamente.'
 		SELECT @error
-		RETURN 1
-	END CATCH
-END
-IF @flag = 2 -- READ
-BEGIN
-	SELECT P.idProducto, nombre, precio, imagen, cuidados, color, tipoMaterial, altura, longitud, profundidad, tipoProducto
-	FROM Producto AS P
-	INNER JOIN Caracteristicas
-		ON P.idProducto = Caracteristicas.idProducto
-	INNER JOIN TipoProducto
-		ON P.idTipoProducto = TipoProducto.idTipoProducto
-	INNER JOIN TipoMaterial
-		ON P.idTipoMaterial = TipoMaterial.idTipoMaterial
-	INNER JOIN Dimensiones
-		ON P.idDimensiones = Dimensiones.idDimensiones
-	WHERE P.idProducto = ISNULL(@idProducto,P.idProducto) 
-	AND   P.idTipoMaterial = ISNULL(@idTipoMaterial,P.idTipoMaterial)
-	AND   P.idTipoProducto = ISNULL(@idTipoProducto,P.idTipoProducto)
-	AND   P.idDimensiones = ISNULL(@idDimensiones,P.idDimensiones)
-	AND   precio = ISNULL(@precio,precio)
-	AND   nombre = ISNULL(@nombre,nombre)
-	AND   color = ISNULL(@color,color)
-	AND   cuidados = ISNULL(@cuidados,cuidados)
-	SET @error = 'Consulta finalizada.'
-	SELECT @error
-END
-IF @flag = 3 -- UPDATE
-BEGIN
-	UPDATE Producto 
-	SET   idTipoMaterial = ISNULL(@idTipoMaterial,idTipoMaterial),
-	      idTipoProducto = ISNULL(@idTipoProducto,idTipoProducto),
-	      idDimensiones = ISNULL(@idDimensiones,idDimensiones),
-	      precio = ISNULL(@precio,precio),
-	      nombre = ISNULL(@nombre,nombre),
-	      cuidados = ISNULL(@cuidados,cuidados),
-		  imagen = @imagen
-	WHERE idProducto = @idProducto
-	UPDATE Caracteristicas 
-	SET   color = ISNULL(@color,color)
-	WHERE idProducto = @idProducto
-	SET @error = 'El producto se ha actualizado correctamente.'
-	SELECT @error
-END
-IF @flag = 4 -- DELETE
-BEGIN
-	DELETE FROM Caracteristicas
-	WHERE idProducto = @idProducto
-	DELETE FROM Producto 
-	WHERE idProducto = @idProducto
-	SET @error = 'El producto se ha eliminado correctamente.'
-	SELECT @error
+	END
+	IF @flag = 4 -- DELETE
+	BEGIN
+		DELETE FROM Caracteristicas
+		WHERE idProducto = @idProducto
+		DELETE FROM Producto 
+		WHERE idProducto = @idProducto
+		SET @error = 'El producto se ha eliminado correctamente.'
+		SELECT @error
 	
-END
+	END
+/*END TRY
+BEGIN CATCH               
+    INSERT INTO dbo.Errores    VALUES (
+        SUSER_SNAME(),
+        ERROR_NUMBER(),
+        ERROR_STATE(),
+        ERROR_SEVERITY(),
+        ERROR_LINE(),
+        ERROR_PROCEDURE(),
+        ERROR_MESSAGE(),
+        GETDATE()
+    );
+END CATCH*/
 PRINT(@error)
 RETURN 0
 
@@ -103,7 +119,7 @@ EXECUTE spProducto 4,2,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL
 SELECT * FROM Producto
 SELECT * FROM Caracteristicas
 
-*/
+
 DECLARE @imagenAlimento VARBINARY(MAX);
 SET @imagenAlimento = (SELECT BulkColumn FROM OPENROWSET(BULK 'C:/abeja.jpg',SINGLE_BLOB) AS Imagen)
 EXECUTE spProducto 1,1,1,3,2,350000,'Mueble de cocina',@imagenAlimento,'Negro','Utilizar un paño suave para limpiarlos y darles brillo, utilizar productos en aerosol.'
@@ -111,3 +127,5 @@ EXECUTE spProducto 1,1,1,3,2,350000,'Mueble de cocina',@imagenAlimento,'Negro','
 SELECT * FROM Producto
 
 EXECUTE spProducto 4,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL
+
+*/
--- SP/ConsultarTipoProducto.sql
+++ SP/ConsultarTipoProducto.sql
@@ -5,35 +5,49 @@ USE Muebleria
 
 /* Funcion Consulta con parametros opcionales: 
 Un cliente puede ver los productos existentes, consultar por tipo de mueble, 
-o verlos todos, se debe mostrar la imagen del producto, las caracter�sticas, 
-precio, y dem�s informaci�n*/
+o verlos todos, se debe mostrar la imagen del producto, las características, 
+precio, y demás información*/
 
 GO
 ALTER PROCEDURE spProductosExistentes (@idTipoProducto int)
 WITH ENCRYPTION AS
 DECLARE @error varchar(2222)
-BEGIN
-	SELECT P.idProducto, 
-		nombre, 
-		precio, 
-		imagen, 
-		cuidados, 
-		color, 
-		tipoMaterial, 
-		altura, 
-		longitud, 
-		profundidad
-	FROM Producto AS P
-	INNER JOIN Caracteristicas
-		ON P.idProducto = Caracteristicas.idProducto
-	INNER JOIN TipoProducto
-		ON P.idTipoProducto = TipoProducto.idTipoProducto
-	INNER JOIN TipoMaterial
-		ON P.idTipoMaterial = TipoMaterial.idTipoMaterial
-	INNER JOIN Dimensiones
-		ON P.idDimensiones = Dimensiones.idDimensiones
-	WHERE P.idTipoProducto = ISNULL(@idTipoProducto,P.idTipoProducto)
-END
+BEGIN TRY
+	BEGIN
+		SELECT P.idProducto, 
+			nombre, 
+			precio, 
+			imagen, 
+			cuidados, 
+			color, 
+			tipoMaterial, 
+			altura, 
+			longitud, 
+			profundidad
+		FROM Producto AS P
+		INNER JOIN Caracteristicas
+			ON P.idProducto = Caracteristicas.idProducto
+		INNER JOIN TipoProducto
+			ON P.idTipoProducto = TipoProducto.idTipoProducto
+		INNER JOIN TipoMaterial
+			ON P.idTipoMaterial = TipoMaterial.idTipoMaterial
+		INNER JOIN Dimensiones
+			ON P.idDimensiones = Dimensiones.idDimensiones
+		WHERE P.idTipoProducto = ISNULL(@idTipoProducto,P.idTipoProducto)
+	END
+END TRY
+BEGIN CATCH               
+    INSERT INTO dbo.Errores    VALUES (
+        SUSER_SNAME(),
+        ERROR_NUMBER(),
+        ERROR_STATE(),
+        ERROR_SEVERITY(),
+        ERROR_LINE(),
+        ERROR_PROCEDURE(),
+        ERROR_MESSAGE(),
+        GETDATE()
+    );
+END CATCH
 RETURN 0
 
 EXECUTE spProductosExistentes 2
